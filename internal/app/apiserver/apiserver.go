20841c112d07bc8fab49827cfd46fc4ac21eff90
Maksim <maxim2000shev@gmail.com>
2022-02-21 12:41:46 +0300

    addedversion

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
-	"github.com/vlasove/8.HandlerImpl2/internal/app/midleware"
 	store "github.com/vlasove/8.HandlerImpl2/store1"
 )
 
@@ -57,17 +56,17 @@ func (s *APIServer) configureLogger() error {
 
 //func for configure Router
 func (s *APIServer) configureRouter() {
+	s.router.HandleFunc(prefix+"/articles", s.GetAllArticles).Methods("GET")
+	s.router.HandleFunc(prefix+"/articles"+"/{id}", s.GetArticleById).Methods("GET")
+	s.router.HandleFunc(prefix+"/articles"+"/{id}", s.DeleteArticleById).Methods("DELETE")
+	s.router.HandleFunc(prefix+"/articles", s.PostArticle).Methods("POST")
+
+	s.router.HandleFunc(prefix+"/user/register", s.PostUserRegister).Methods("POST")
+	//router companies
 	s.router.HandleFunc(prefix+"/companies", s.GetAllCompanies).Methods("GET")
-	//s.router.HandleFunc(prefix+"/articles"+"/{id}", s.GetArticleById).Methods("GET")
-	s.router.Handle(prefix+"//companies"+"/{id}", midleware.JwtMiddleware.Handler(
-		http.HandlerFunc(s.GetCompanyById),
-	)).Methods("GET")
+	s.router.HandleFunc(prefix+"/companies"+"/{id}", s.GetCompanyById).Methods("GET")
 	s.router.HandleFunc(prefix+"/companies"+"/{id}", s.DeleteCompanyById).Methods("DELETE")
 	s.router.HandleFunc(prefix+"/companies", s.PostCompany).Methods("POST")
-	s.router.HandleFunc(prefix+"/user/register", s.PostUserRegister).Methods("POST")
-
-	s.router.HandleFunc(prefix+"/user/auth", s.PostToAuth).Methods("POST")
-
 }
 
 //configureStore method
